d4631f0c9c5790e78afabd339a275e91db68abcb
JosiahFu <josiahfu@gmail.com>
2022-09-09 17:12:59 -0700

    Constants and Controllers

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,9 @@
 
 package frc.robot;
 
+
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -12,4 +15,10 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public final class USB {
+        public static final int leftJoystick = 0;
+        public static final int rightJoystick = 1;
+        public static final int xBoxController = 2;
+    }
+}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,11 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.POVButton;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -17,10 +21,16 @@ import edu.wpi.first.wpilibj2.command.Command;
  * subsystems, commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  static Joystick leftJoystick = new Joystick(Constants.USB.leftJoystick);
+  static Joystick rightJoystick = new Joystick(Constants.USB.rightJoystick);
+  static XboxController xBoxController = new XboxController(Constants.USB.xBoxController);
+
+  public Button[] leftButtons = new Button[2];
+  public Button[] rightButtons = new Button[2];
+  public Button[] xBoxButtons = new Button[10];
+  public Button[] xBoxPOVButtons = new Button[4];
+  public Button xBoxLeftTrigger, xBoxRightTrigger;
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
@@ -34,7 +44,16 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    for (int i = 0; i < leftButtons.length; i++)
+      leftButtons[i] = new JoystickButton(leftJoystick, (i + 1));
+    for (int i = 0; i < rightButtons.length; i++)
+      rightButtons[i] = new JoystickButton(rightJoystick, (i + 1));
+    for (int i = 0; i < xBoxButtons.length; i++)
+      xBoxButtons[i] = new JoystickButton(xBoxController, (i + 1));
+    for (int i = 0; i < xBoxPOVButtons.length; i++)
+      xBoxPOVButtons[i] = new POVButton(xBoxController, (i * 90));
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
@@ -43,6 +62,6 @@ public class RobotContainer {
    */
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
-    return m_autoCommand;
+    return null;
   }
 }
