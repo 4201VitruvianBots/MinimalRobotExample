20548f0d622d2487443fd797a7958dae6fec7b70
JosiahFu <josiahfu@gmail.com>
2022-09-09 17:03:45 -0700

    Drivetrain template

---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,51 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DriveTrain extends SubsystemBase {
+  // Initialize the motors
+
+
+  /** Creates a new DriveTrain. */
+  public DriveTrain() {
+
+  }
+
+  public void configureCTREMotors() {
+
+  }
+
+  public void setVoltageOutput(/*Parameters*/) {
+
+  }
+
+  public void setWheelSpeeds(/*Parameters*/) {
+
+  }
+
+  public void setMotorPercentOutput(/*Parameters*/) {
+
+  }
+
+  public void setMotorArcadeDrive(/*Parameters*/) {
+
+  }
+
+  public void setMotorTankDrive(/*Parameters*/) {
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
