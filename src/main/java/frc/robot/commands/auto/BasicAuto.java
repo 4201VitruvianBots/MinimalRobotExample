c94748080c5be6421261e717bb6c8c6bc332cf93
JosiahFu <josiahfu@gmail.com>
2022-09-09 18:05:50 -0700

    Basic autO

---
+++ src/main/java/frc/robot/commands/auto/BasicAuto.java
@@ -0,0 +1,19 @@
+package frc.robot.commands.auto;
+
+import com.pathplanner.lib.PathPlanner;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+
+public class BasicAuto extends SequentialCommandGroup {
+
+    public BasicAuto(/*Parameters*/) {
+        // Create trajectory
+
+        // Create Vitruvian Ramsete Command
+
+
+        addCommands(
+            // Add commands
+        );
+
+    }
+}
