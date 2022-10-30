ace4f4a13e1c47057227e12c50b8ee2831e805df
Zirak <zirakbe@gmail.com>
2022-10-30 15:38:07 -0700

    untested turret code

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -13,17 +13,44 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboardTab;
+
 import frc.robot.Constants;
-import frc.robot.Constants.CONTROL_MODE;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.XboxController;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.SparkMaxAlternateEncoder;
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+
+
+
 
 public class Turret extends SubsystemBase {
-  /** Creates a new Turret. */
-  public Turret() {}
-
+  private boolean direction,directionTripped,joystickMoved;
+  private final XboxController m_controller;
+  private static final int kCanID = 1;
+  private static final MotorType kMotorType = MotorType.kBrushless;
+  private final CANSparkMax m_motor;
+  private SparkMaxPIDController m_pidController;
+  public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
+  public Turret() {
+    m_motor = new CANSparkMax(kCanID, kMotorType);
+    m_motor.restoreFactoryDefaults();
+    m_controller = new XboxController(2);
+  
+  }
+  
+  public void setPercentOutput(){
+    m_motor.set(m_controller.getRawAxis(1));
+  };
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    
   }
 }
